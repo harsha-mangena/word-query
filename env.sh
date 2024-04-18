57be01c2761f3638884e92df5e9f763fcbd66f21
harsha-mangena <44197358+harsha-mangena@users.noreply.github.com>
2024-04-18 00:30:59 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,1 @@
-export DJANGO_SECRET_KEY='django-insecure-liz)7h4ch%z-@7qb&#qu&*xxb_*3a_18^m91x%ynj640vq2h3o'
-export DB_ENGINE='djongo'
-export DB_NAME='word_query'
-export DB_PORT=27017
-export DB_HOST='localhost'
+//Data goes here!
